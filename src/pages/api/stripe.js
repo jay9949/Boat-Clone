1e33b9661f78c9760c28de14ba1e10afd5027dc4
jay9949 <jay37192@gmail.com>
2024-01-08 17:48:12 +0530

    #

---
+++ src/pages/api/stripe.js
@@ -0,0 +1,57 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body.cartItems);
+
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1OWF02SHdiTIySw6oNdVvfXg" },
+          { shipping_rate: "shr_1OWF73SHdiTIySw6eYdZJTCo" },
+        ],
+        line_items: req.method.cartItems.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/3u1nn9f4/production/"
+            )
+            .replace("-webp", ".webp");
+
+          return {
+            price_data: {
+              currency: "INR",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minmum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+
+      const session = await stripe.checkout.sessions.create(params);
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
